389e9d18d3e84fb57d762d60d6afd118c30095f5
Smork <daniil.tabakov004@gmail.com>
2023-11-29 17:51:11 +0100

    Добавьте файлы проекта.

---
+++ Lab4_c/Drob.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_c
+{
+    internal class Drob
+    {
+        private int _nominator; private int _denominator;
+        public int Nominator
+        {
+            get { return _nominator; }
+            set { _nominator = value; }
+        }
+        public int Denominator
+        {
+            get { return _denominator; }
+            set { _denominator = value != 0 ? value : throw new FormatException("Denominator kann nicht 0 sein."); }
+        }
+        public Drob(int nominator, int denominator)
+        {
+            Nominator = nominator;
+            Denominator = denominator;
+        }
+        public override string ToString() { return $"{Nominator}/{Denominator}"; }
+        public static Drob operator +(Drob a, Drob b)
+        {
+            return (a.Denominator != b.Denominator) ? new Drob(a.Nominator * b.Denominator + b.Nominator * a.Denominator, a.Denominator * b.Denominator)
+                : new Drob(a.Nominator + b.Nominator, a.Denominator);
+        }
+        public static Drob operator -(Drob a, Drob b)
+        {
+            return (a.Denominator != b.Denominator) ? new Drob(a.Nominator * b.Denominator - b.Nominator * a.Denominator, a.Denominator * b.Denominator) : new Drob(a.Nominator - b.Nominator, a.Denominator);
+        }
+        public static Drob operator *(Drob a, Drob b)
+        {
+            return new Drob(a.Nominator * b.Nominator, a.Denominator * b.Denominator);
+        }
+        public static Drob operator /(Drob a, Drob b)
+        {
+            return new Drob(a.Nominator * b.Denominator, a.Denominator * b.Nominator);
+        }
+        public static bool operator >(Drob a, Drob b)
+        {
+            return a.Nominator * b.Denominator > b.Nominator * a.Denominator;
+        }
+        public static bool operator <(Drob a, Drob b)
+        {
+            return a.Nominator * b.Denominator < b.Nominator * a.Denominator;
+        }
+        public Drob pow(int k)
+        {
+            Drob answer = new Drob(1, 1);
+            for (int i = 0; i < k; i++)
+            {
+                answer.Nominator *= this.Nominator; answer.Denominator *= this.Denominator;
+            }
+            return answer;
+        }
+    }
+}
---
+++ Lab4_c/Program.cs
@@ -0,0 +1,15 @@
+﻿using Lab4_c;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Drob d1 = new Drob(3, 7); Drob d2 = new Drob(2, 5);
+        Drob d3 = new Drob(1, 2);
+        Console.WriteLine((d3 - d1) * d3); 
+        Console.WriteLine((d1 + d2) / d1.pow(3));
+        Console.WriteLine(d1 < d2); 
+        Console.WriteLine(d2 < d3);
+        Console.WriteLine(d1 > d3);
+    }
+}
